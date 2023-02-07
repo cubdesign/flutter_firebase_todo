db272315fc36eb13913d3370997141c0a53bd987
cubdesign <takeo@cubdesign.com>
2023-02-07 17:04:17 +0900

    add fake_cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_todo/add_task_alert_dialog.dart';
 import 'package:flutter_firebase_todo/app_config.dart';
+import 'package:flutter_firebase_todo/firebase_options.dart';
 import 'package:flutter_firebase_todo/home_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
 
   final tags = await fetchTags();
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,35 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_todo/firebase_options.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_firebase_todo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    final firestore = FakeFirebaseFirestore();
+
+    await firestore
+        .collection("task-tags")
+        .add({'taskName': "タスク名", 'taskDesc': "説明", 'taskTag': "Work"});
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(const TodoApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
