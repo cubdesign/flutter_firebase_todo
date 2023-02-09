92862811c3c97a2e80ebb36f7ed57de4d112f0e0
cubdesign <takeo@cubdesign.com>
2023-02-09 22:10:19 +0900

    add test for tasks widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -18,27 +19,34 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     final firestore = FakeFirebaseFirestore();
 
-    await firestore
-        .collection("task-tags")
+    DocumentReference docRef = await firestore
+        .collection("tasks")
         .add({'taskName': "タスク名", 'taskDesc': "説明", 'taskTag': "Work"});
+
+    String taskId = docRef.id;
+    await firestore.collection('tasks').doc(taskId).update(
+      {'id': taskId},
+    );
 
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
-      home: Tasks(
-        firestore: firestore,
+      home: Scaffold(
+        body: Tasks(
+          firestore: firestore,
+        ),
       ),
     ));
 
-    // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+    expect(find.text('No tasks to display'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    // Let the snapshots stream fire a snapshot.
+    await tester.idle();
+    // Re-render.
+    await tester.pump();
 
-    // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    expect(find.text('No tasks to display'), findsNothing);
+
+    expect(find.text('タスク名'), findsOneWidget);
+    expect(find.text('説明'), findsOneWidget);
   });
 }
